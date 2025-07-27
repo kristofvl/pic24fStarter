b465bb35753d6d7016f266690d607cde9c128d1f
Kristof Van Laerhoven <kryztov@gmail.com>
2025-07-27 09:38:56 +0200

    updated with MPLabX files

---
+++ build/default/debug/main.o.d
@@ -0,0 +1,5 @@
+build/default/debug/main.o: main.c PIC24FStarter.h SH1101A.h
+
+PIC24FStarter.h:
+
+SH1101A.h:
---
+++ build/default/production/main.o.d
@@ -0,0 +1,5 @@
+build/default/production/main.o: main.c PIC24FStarter.h SH1101A.h
+
+PIC24FStarter.h:
+
+SH1101A.h:
